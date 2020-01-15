bebfda31dd27d272d62c7bf8ba2df9d9a35ad883
Dmitri12 <47256181+Dmitri12@users.noreply.github.com>
2020-01-15 14:40:48 -0500

    Update subproc.py
    Added iteration of COM port parameter

--- subproc.py
+++ subproc.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
+import serial
 import sys
 import subprocess
+
+ports = []
+    #Generates list of available COM ports
 s = subprocess.check_output(["python", "-m", "serial.tools.list_ports"])
 output = s.decode("utf-8")
 lines = output.split('\n')
 for line in lines:
     print(line)
+    ports.append(line)
+    
+    #Iterates and prints parameters of available ports
+ser = serial.Serial()
+for i in ports:
+    ser.port = i
+    print(ser)
